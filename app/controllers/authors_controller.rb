7d84f84e5f2cf51d1c6f15497d95c86d792d1782
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-20 20:52:21 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,7 @@
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
+    redirect_to author_path(@author)
   end
 
   def new
@@ -8,9 +9,24 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render "new"
+    end
+  end
 
-    redirect_to author_path(@author)
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    if @author = Author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render "edit"
+    end
   end
 
   private
