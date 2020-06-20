3563d84ab08f0db6841b1e8bee0b9fd11f336c22
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-20 20:52:49 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    redirect_to post_path(@post)
   end
 
   def edit
@@ -10,9 +11,12 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    if @post.update(post_params)
 
-    redirect_to post_path(@post)
+      redirect_to post_path(@post)
+    else
+      render "edit"
+    end
   end
 
   private
